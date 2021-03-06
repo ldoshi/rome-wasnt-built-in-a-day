09e73df0ac3cad361ae34c1e7bab6aa306700dea
arvindthiagarajan <arvindthiagarajan@gmail.com>
2021-03-06 03:10:37 -0500

    Small opts for sumtree + prepping it for multiprocessing

--- bridge-builder/replay_buffer.py
+++ bridge-builder/replay_buffer.py
@@ -1,133 +1,150 @@
 import numpy as np
 import random
 from bisect import bisect_right
-from collections import deque
+from itertools import chain, starmap
+from functools import partial
 
 # The sum tree is contained in a single array. The slots represent the
 # layers of the binary tree starting from the root laid out
 # contiguously. The intermediate layers contains the sum of priorities
 # of its children. The leaf nodes contain the priorities stored in the
 # tree.
-# An example tree with capacity 7. 
+# An example tree with capacity 7.
 # [10,  6, 4,  2, 4, 2, 2,  1, 1, 3, 1, 1, 1, 2, _ ]
 # |L0 |  L1  |     L2     |          L3          ^ unused slot.
 
-def layer_base(depth):
-    return (1 << depth) - 1
 
 class SumTree:
-  def __init__(self, capacity):      
-      self._tree_depth = np.ceil(np.log2(capacity)).astype(int) + 1
-      self._tree = np.zeros(1 << self._tree_depth)
-      self._capacity = capacity
-      self.max_value = 1
+    def __init__(self, capacity):
+        """Creates a SumTree for storing up to `capacity` leaf values"""
+        self._tree_depth = np.ceil(np.log2(capacity)).astype(int) + 1
+        self._leaf_base = (1 << (self._tree_depth - 1)) - 1
+        self._tree = np.zeros(2 * self._leaf_base + 1)
+        self._capacity = capacity
+        self.max_value = 1
 
-  # TODO(lyric): Delete this.
-  def debug(self):
-      print(self._tree_depth)
-      print(self._tree)
-      print(self._capacity)
-      print(self.max_value)
-      
-  # By construction, the root node contains the sum of all values.
-  def _tree_total_value(self):
-      return self._tree[0]
+    # TODO(lyric): Delete this.
+    def debug(self):
+        print(self._tree_depth)
+        print(self._tree)
+        print(self._capacity)
+        print(self.max_value)
+
+    def __getitem__(self, index):
+        return self._tree[index]
+
+    # By construction, the root node contains the sum of all values.
+    @property
+    def _tree_total_value(self):
+        return self[0]
+
+    def _process_layer(
+        self,
+        target_values,
+        target_values_left,
+        target_values_right,
+        target_index,
+        current_left_sum,
+    ):
+
+        cap_err = f"Target Index {target_index} vs Capacity {self._capacity}"
+
+        leaf_index = target_index - self._leaf_base
+        assert leaf_index < self._capacity, cap_err
+
+        if leaf_index >= 0:
+            # The value (target_values_right - target_values_left)
+            # represents how many times we selected this element
+            # (sampling with replacement).
+            sample = (leaf_index, self[target_index] / self._tree_total_value)
+            return [sample] * (target_values_right - target_values_left)
+
+        target_index = 2 * target_index + 1
+        left_sum = self[target_index] + current_left_sum
+        split_point = bisect_right(
+            target_values, left_sum, target_values_left, target_values_right
+        )
+
+        next_level_splits = []
+        # Traverse left.
+        if target_values_left < split_point:
+            next_level_splits.append(
+                (target_values_left, split_point, target_index, current_left_sum)
+            )
+
+        # Traverse right.
+        if split_point < target_values_right:
+            next_level_splits.append(
+                (split_point, target_values_right, target_index + 1, left_sum)
+            )
+
+        return next_level_splits
+
+    def _sample_collector(self, target_values):
+
+        # Each tuple in splits contains (target_values_left,
+        # target_values_right, target_index, current_left_sum).
+        layer_func = partial(self._process_layer, target_values)
+        splits = [(0, len(target_values), 0, 0)]
+        for i in range(self._tree_depth):
+            # With multiple cores, you can split out itertools.starmap
+            # for a multiprocessing version
+            splits = chain.from_iterable(starmap(layer_func, splits))
+        return list(splits)
+
+    # Samples with replacement. Returns the index and probability of n
+    # elements. If stratified is true, the selection space [0,
+    # tree_total_value) is split into n contiguous buckets of size 1/n
+    # and a sample if uniformly chosen from within each bucket.
+    def sample(self, n, stratified=False):
+        if n == 0:
+            return []
+
+        if stratified:
+            # Sorted by construction.
+            target_values = (np.random.rand(n) + np.arange(n)) / n
+        else:
+            target_values = np.sort(np.random.rand(n))
+
+        return self._sample_collector(target_values * self._tree_total_value)
+
+    def set_value(self, index, value):
+        if index >= self._capacity:
+            raise ValueError(
+                f"Attempted to set index {index} beyond capacity {self._capacity}."
+            )
+
+        self.max_value = max(value, self.max_value)
+
+        index += self._leaf_base
+        delta = value - self._tree[index]
+
+        # Update nodes from leaf to root to adjust for the delta in the
+        # value at index.
+        while index >= 0:
+            self._tree[index] += delta
+            index = (index - 1) // 2
 
 
-  def _sample_collector(self, target_values):
-      samples = []
-      
-      # Each tuple in traversal contains (target_values_left,
-      # target_values_right, target_index, current_depth).
-      traversal = deque([(0, len(target_values), 0, 0)])
-
-      while traversal:
-          current = traversal.pop()
-
-          assert current[2] < self._capacity, "Target Index {} vs Capacity {}".format(current[2], self._capacity)
-      
-          if current[3] == self._tree_depth:
-              # The value (target_values_right - target_values_left)
-              # represents how many times we selected this element
-              # (sampling with replacement).
-              for _ in range(current[1] - current[0]):
-                  samples.append((current[2], self._tree[layer_base(self._tree_depth - 1) + current[2]] / self._tree_total_value()))
-              continue
-
-          updated_target_index = current[2] * 2
-          left_sum = self._tree[layer_base(current[3]) + updated_target_index]
-          split_point = bisect_right(target_values, left_sum, current[0], current[1])
-          # Traverse left.
-          if current[0] < split_point:
-              traversal.append((current[0], split_point, updated_target_index, current[3] + 1))
-
-          # Traverse right.
-          if split_point < current[1]:
-              target_values[split_point:current[1]] -= left_sum
-              traversal.append((split_point, current[1], updated_target_index + 1, current[3] + 1))
-
-      return samples
-          
-  # Samples with replacement. Returns the index and probability of n
-  # elements. If stratified is true, the selection space [0,
-  # tree_total_value) is split into n contiguous buckets of size 1/n
-  # and a sample if uniformly chosen from within each bucket.
-  def sample(self, n, stratified=False):
-      if n == 0:
-          return []
-
-      if stratified:
-          boundaries = np.linspace(0,1,n+1) * self._tree_total_value()
-          # Sorted by construction.
-          target_values = [random.uniform(boundaries[i],boundaries[i+1]) for i in range(len(boundaries) - 1)] 
-      else:
-          target_values = [random.uniform(0,1) * self._tree_total_value() for _ in range(n)]
-          target_values.sort()
-
-      target_values = np.array(target_values)
-      return self._sample_collector(target_values)
-            
-  def set_value(self, index, value):
-      if index >= self._capacity:
-          raise ValueError('Attempted to set index {} beyond capacity {}.'.
-                           format(index, self._capacity))
-
-      self.max_value = max(value, self.max_value)
-      
-
-      current_depth_base = self._tree_depth - 1
-      current_index = index
-
-      delta = value - self._tree[layer_base(current_depth_base) + current_index]
-
-      # Update nodes from leaf to root to adjust for the delta in the
-      # value at index.
-      while current_depth_base > -1:
-          self._tree[layer_base(current_depth_base) + current_index] += delta
-
-          current_depth_base -= 1
-          current_index //= 2
-        
 class ReplayBuffer:
-    def __init__(self, capacity, alpha=.5):
+    def __init__(self, capacity, alpha=0.5):
         self._size = n
         self._content = []
         self._index = 0
-  
+
     def add(self, s, a, ss, r, d):
-        if len(self._content) == self._size:    
+        if len(self._content) == self._size:
             self._content[self._index] = [s, a, ss, r, d]
             self._index = (self._index + 1) % self._size
-        else:                      
+        else:
             self._content.append([s, a, ss, r, d])
-  
-    def sample(self, n, beta):           
-      samples = random.sample(self._content, min(n, len(self._content)))
 
-      states = np.array([i[0] for i in samples]) 
-      actions = np.array([i[1] for i in samples])
-      next_states = np.array([i[2] for i in samples])
-      rewards = np.array([[i[3]] for i in samples])
-      is_dones = np.array([[i[4]] for i in samples])
-      return states, actions, next_states, rewards, is_dones
-    
+    def sample(self, n, beta):
+        samples = random.sample(self._content, min(n, len(self._content)))
+
+        states = np.array([i[0] for i in samples])
+        actions = np.array([i[1] for i in samples])
+        next_states = np.array([i[2] for i in samples])
+        rewards = np.array([[i[3]] for i in samples])
+        is_dones = np.array([[i[4]] for i in samples])
+        return states, actions, next_states, rewards, is_dones
--- bridge-builder/replay_buffer_test.py
+++ bridge-builder/replay_buffer_test.py
@@ -1,55 +1,81 @@
 import numpy as np
 import unittest
+from timeit import timeit
+from parameterized import parameterized
+
 from replay_buffer import SumTree
 
+
 class TestSumTree(unittest.TestCase):
-    
     def setUp(self):
         self._delta = 1e-6
-    
+
+    @parameterized.expand(
+        [
+            ("Stratified", True, 100000, 100, 10000),
+            ("Not Stratified", False, 100000, 100, 10000),
+        ]
+    )
+    def test_sampling_speed(self, name, stratified, capacity, batch, repititions):
+        setup = [
+            "import random",
+            "from replay_buffer import SumTree",
+            f"tree=SumTree({capacity})",
+            f"for i in range({capacity}): tree.set_value(i, random.uniform(0, 10))",
+        ]
+
+        time = timeit(
+            stmt=f"tree.sample({batch}, stratified={stratified})",
+            setup="\n".join(setup),
+            number=repititions,
+        )
+        report = f"{name} Runtime: {time} sec for {repititions} repititions "
+        report += f"sampling batches of {batch} with a buffer capacity of {capacity}"
+        print(report)
+
     def test_illegal_tree_usage(self):
         tree = SumTree(1)
         self.assertRaises(ValueError, tree.set_value, 1, 1)
 
-    def test_small_tree_size(self):
+    @parameterized.expand(
+        [
+            ("No Sampling", 0, []),
+            ("Single Sample", 1, [(0, 1)]),
+            ("Three Samples", 3, [(0, 1), (0, 1), (0, 1)]),
+        ]
+    )
+    def test_small_tree_size(self, name, batch, expected):
+        tree = self._get_small_tree()
+        self.assertListEqual(tree.sample(batch), expected)
+
+    @parameterized.expand(
+        [
+            ("Uniform -> Prioritized", 21, {}, {1: 7, 19: 7}),
+            ("Prioritized -> Uniform", 21, {1: 7, 19: 7}, {}),
+        ]
+    )
+    def test_priority_switching(
+        self, name, capacity, first_priorities, second_priorities
+    ):
+        tree = SumTree(capacity)
+        self._populate_sum_tree(tree, capacity, first_priorities)
+
+        self._sample_and_verify(tree, capacity, first_priorities, stratified=False)
+        self._sample_and_verify(tree, capacity, first_priorities, stratified=True)
+
+        # Switch priorities
+        self._populate_sum_tree(
+            tree, capacity, second_priorities, max_value=tree.max_value
+        )
+
+        self._sample_and_verify(tree, capacity, second_priorities, stratified=False)
+        self._sample_and_verify(tree, capacity, second_priorities, stratified=True)
+
+    def _get_small_tree(self):
         tree = SumTree(1)
         tree.set_value(0, 10)
         self.assertEqual(tree.max_value, 10)
-        self.assertListEqual(tree.sample(0), [])
-        self.assertListEqual(tree.sample(1), [(0, 1)])
-        self.assertListEqual(tree.sample(3), [(0, 1), (0, 1), (0, 1)])
-
-    def test_uniform_tree(self):
-        capacity = 21
-        tree = SumTree(capacity)
-        boosted_priorities={}
-        self._populate_sum_tree(tree, capacity, boosted_priorities)
-
-        self._sample_and_verify(tree, capacity, boosted_priorities, stratified=False)
-        self._sample_and_verify(tree, capacity, boosted_priorities, stratified=True)
-
-        # Set the values from uniform trees to instead have some boosted priorities.
-        boosted_priorities={1: 7, 19: 7}
-        self._populate_sum_tree(tree, capacity, boosted_priorities)
-
-        self._sample_and_verify(tree, capacity, boosted_priorities, stratified=False)
-        self._sample_and_verify(tree, capacity, boosted_priorities, stratified=True)
-
-
-    def test_prioritized_tree(self):
-        capacity = 21
-        tree = SumTree(capacity)
-        boosted_priorities={1: 7, 19: 7}
-        self._populate_sum_tree(tree, capacity, boosted_priorities)
-
-        self._sample_and_verify(tree, capacity, boosted_priorities, stratified=False)
-        self._sample_and_verify(tree, capacity, boosted_priorities, stratified=True)
-            
-        # Set the values from boosted_priorities back to 1 and verify the tree is now uniform.
-        self._populate_sum_tree(tree, capacity, boosted_priorities={}, max_value=tree.max_value)
-
-        self._sample_and_verify(tree, capacity, boosted_priorities={}, stratified=False)
-        self._sample_and_verify(tree, capacity, boosted_priorities={}, stratified=True)
+        return tree
 
     def _populate_sum_tree(self, tree, capacity, boosted_priorities, max_value=1):
         for i in range(capacity):
@@ -58,7 +84,7 @@ class TestSumTree(unittest.TestCase):
                 max_value = max(max_value, boosted_priorities[i])
             else:
                 tree.set_value(i, 1)
-                
+
             self.assertEqual(tree.max_value, max_value)
 
     def _sample_and_verify(self, tree, capacity, boosted_priorities, stratified):
@@ -74,7 +100,7 @@ class TestSumTree(unittest.TestCase):
         sample_index_sets = [set() for _ in range(sample_count)]
         repeat_found = False
 
-        lower_priority_numerator = (capacity - len(boosted_priorities))
+        lower_priority_numerator = capacity - len(boosted_priorities)
         higher_priority_numerator = np.sum(np.array(list(boosted_priorities.values())))
 
         counts = np.zeros(capacity)
@@ -87,17 +113,26 @@ class TestSumTree(unittest.TestCase):
             # cases are constructed to ensure no overlap instead of
             # little overlap.
             repeat_found |= len(set([element[0] for element in sample])) != len(sample)
-            
+
             for i, element in enumerate(sample):
                 if element[0] in boosted_priorities:
-                    self.assertAlmostEqual(element[1],
-                                           (higher_priority_numerator / (lower_priority_numerator + higher_priority_numerator)) * 1 / len(boosted_priorities),
-                                           delta=self._delta)
+                    self.assertAlmostEqual(
+                        element[1],
+                        (
+                            higher_priority_numerator
+                            / (lower_priority_numerator + higher_priority_numerator)
+                        )
+                        * 1
+                        / len(boosted_priorities),
+                        delta=self._delta,
+                    )
                 else:
-                    self.assertAlmostEqual(element[1],
-                                           (1 / (lower_priority_numerator + higher_priority_numerator)),
-                                           delta=self._delta)
-                    
+                    self.assertAlmostEqual(
+                        element[1],
+                        (1 / (lower_priority_numerator + higher_priority_numerator)),
+                        delta=self._delta,
+                    )
+
                 counts[element[0]] += 1
                 sample_index_sets[i].add(element[0])
 
@@ -106,26 +141,46 @@ class TestSumTree(unittest.TestCase):
             # Verify that all sample_index_sets are disjoint.
             for i in range(len(sample_index_sets)):
                 for j in range(i):
-                    self.assertEqual(len(sample_index_sets[i].intersection(sample_index_sets[j])), 0)
+                    self.assertEqual(
+                        len(sample_index_sets[i].intersection(sample_index_sets[j])), 0
+                    )
         else:
             self.assertTrue(repeat_found)
             # Verify that intersections exist among sample_index_sets.
             for i in range(len(sample_index_sets)):
                 for j in range(i):
-                    self.assertGreater(len(sample_index_sets[i].intersection(sample_index_sets[j])), 0)
-
+                    self.assertGreater(
+                        len(sample_index_sets[i].intersection(sample_index_sets[j])), 0
+                    )
 
         # This is not a very rigorous check. All counts should be
         # within 10% of expected.
-        lower_priority_count = (iterations * sample_count) * (1 / (lower_priority_numerator + higher_priority_numerator))
-        higher_priority_count = (iterations * sample_count) * (higher_priority_numerator / (lower_priority_numerator + higher_priority_numerator)) * 1 / len(boosted_priorities) if len(boosted_priorities) else 0
-        lower_priority_count_delta = lower_priority_count * .1
-        higher_priority_count_delta = higher_priority_count * .1
+        lower_priority_count = (iterations * sample_count) * (
+            1 / (lower_priority_numerator + higher_priority_numerator)
+        )
+        higher_priority_count = (
+            (iterations * sample_count)
+            * (
+                higher_priority_numerator
+                / (lower_priority_numerator + higher_priority_numerator)
+            )
+            * 1
+            / len(boosted_priorities)
+            if len(boosted_priorities)
+            else 0
+        )
+        lower_priority_count_delta = lower_priority_count * 0.1
+        higher_priority_count_delta = higher_priority_count * 0.1
         for i, count in enumerate(counts):
             if i in boosted_priorities:
-                self.assertAlmostEqual(count, higher_priority_count, delta=higher_priority_count_delta)
+                self.assertAlmostEqual(
+                    count, higher_priority_count, delta=higher_priority_count_delta
+                )
             else:
-                self.assertAlmostEqual(count, lower_priority_count, delta=lower_priority_count_delta)
-            
-if __name__ == '__main__':
+                self.assertAlmostEqual(
+                    count, lower_priority_count, delta=lower_priority_count_delta
+                )
+
+
+if __name__ == "__main__":
     unittest.main()
