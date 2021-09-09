e22606fee1a1203f0b704a919c70eacc34632590
arvindthiagarajan <arvindthiagarajan@gmail.com>
2021-09-08 23:38:17 -0400

    Added flag and functionality to load and start from previous checkpoint

--- bridger/config/checkpointing.py
+++ bridger/config/checkpointing.py
@@ -5,6 +5,11 @@ hparam_dict[key] = {"type": str, "default": "checkpoints"}
 help_str = "Path to folder in which model checkpoints will be saved"
 hparam_dict[key]["help"] = help_str
 
+key = "load_checkpoint_path"
+hparam_dict[key] = {"type": str, "default": ""}
+help_str = "Path to file from which to load previously trained model weights"
+hparam_dict[key]["help"] = help_str
+
 key = "checkpoint_interval"
 hparam_dict[key] = {"type": int, "default": 10}
 help_str = "Number of training batch steps per checkpoint creation"
