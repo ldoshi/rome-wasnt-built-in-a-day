d6769b4ebe45e87f60ceadb9259517b79861d919
Lyric Doshi <lyric@mit.edu>
2021-09-19 21:00:23 -0700

    Update DemoCallback constructor arguments.

--- bin/bridge_builder.py
+++ bin/bridge_builder.py
@@ -64,7 +64,6 @@ def test():
                 steps_per_update=MAX_STEPS,
             ),
             DemoCallback(
-                hparams,
                 steps_per_update=MAX_STEPS,
                 max_episode_length=MAX_DEMO_EPISODE_LENGTH,
             ),
