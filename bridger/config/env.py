6e8c16c10065bb5f3635daccdb198dc1facdcbda
arvindthiagarajan <arvindthiagarajan@gmail.com>
2021-10-25 22:26:33 -0700

    Fix argparse bug for env-force-standard-config flag

--- bridger/config/env.py
+++ bridger/config/env.py
@@ -1,3 +1,5 @@
+from argparse import BooleanOptionalAction
+
 hparam_dict = dict()
 
 key = "name"
@@ -11,7 +13,7 @@ help_str = "The width of the environment to be used."
 hparam_dict[key]["help"] = help_str
 
 key = "force_standard_config"
-hparam_dict[key] = {"type": bool, "default": True}
+hparam_dict[key] = {"type": bool, "default": True, "action": BooleanOptionalAction}
 help_str = "Whether to use only the standard environment configuration."
 hparam_dict[key]["help"] = help_str
 
