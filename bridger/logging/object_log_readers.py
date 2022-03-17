21de951ce5b6726967e00ef5b34b70c651d42718
Lyric Doshi <lyric@mit.edu>
2022-03-16 23:28:40 -0700

    stat

--- bridger/logging/object_log_readers.py
+++ bridger/logging/object_log_readers.py
@@ -13,6 +13,7 @@ import shutil
 import pickle
 import os
 import pathlib
+import pandas as pd
 import torch
 
 from collections.abc import Hashable
@@ -32,3 +33,39 @@ def read_object_log(dirname: str, log_filename: str):
 
             except EOFError:
                 break
+
+
+class TrainingHistoryDatabase:
+    """Provides aggregate query access over logged training history.
+
+    The TrainingHistoryDatabase combines the following log entries:
+    * log_entry.TRAINING_HISTORY_VISIT_LOG_ENTRY
+    * log_entry.TRAINING_HISTORY_Q_VALUE_LOG_ENTRY
+    * log_entry.TRAINING_HISTORY_TD_ERROR_LOG_ENTRY
+    * log_entry.STATE_NORMALIZED_LOG_ENTRY
+    """
+
+    def __init__(self, dirname: str):
+        """Loads in training history data in dataframes for querying.
+
+        Args:
+          dirname: The directory containing the training history log files.
+        """
+        self._states = pd.DataFrame(read_object_log(dirname, log_entry.STATE_NORMALIZED_LOG_ENTRY))
+        self._states.set_index('id')
+        self._visits = pd.DataFrame(read_object_log(dirname, log_entry.TRAINING_HISTORY_VISIT_LOG_ENTRY))
+        self._q_values = pd.DataFrame(read_object_log(dirname, log_entry.TRAINING_HISTORY_Q_VALUE_LOG_ENTRY))
+        self._td_errors = pd.DataFrame(read_object_log(dirname, log_entry.TRAINING_HISTORY_TD_ERROR_LOG_ENTRY))
+
+
+    def get_states_by_visit_count(n: int = None) -> pd.DataFrame:
+        return self._visits.groupby(['object' ], sort=False)['batch_idx'].count().reset_index(name='count').sort_values(['count'], ascending=False).head(n).join(self._states, rsuffix='_state').rename(columns={'object_state' : 'state'}).drop(['object'], axis=1)
+
+    def get_q_values(state_ids: pd.DataFrame) -> 
+
+    # potentially add sampling? 
+# rest are given states, get t, q, target
+        
+                
+
+        
