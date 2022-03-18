2a37ff431b16c3279e0c02754ed6ec9fb5ee9b73
Lyric Doshi <lyric@mit.edu>
2022-03-17 20:23:28 -0700

    hi

--- bridger/logging/object_log_readers.py
+++ bridger/logging/object_log_readers.py
@@ -34,7 +34,10 @@ def read_object_log(dirname: str, log_filename: str):
             except EOFError:
                 break
 
-
+# TODO(lyric): Consider adding batch_idx_min and batch_idx_max
+# parameters to the data retrieval functions when the data volume
+# grows enough such that debugging would benefit from a defined
+# tighter window.
 class TrainingHistoryDatabase:
     """Provides aggregate query access over logged training history.
 
@@ -55,16 +58,43 @@ class TrainingHistoryDatabase:
         self._states.set_index('id')
         self._visits = pd.DataFrame(read_object_log(dirname, log_entry.TRAINING_HISTORY_VISIT_LOG_ENTRY))
         self._q_values = pd.DataFrame(read_object_log(dirname, log_entry.TRAINING_HISTORY_Q_VALUE_LOG_ENTRY))
+        self._q_values.set_index('state_id')
         self._td_errors = pd.DataFrame(read_object_log(dirname, log_entry.TRAINING_HISTORY_TD_ERROR_LOG_ENTRY))
+        self._td_errors.set_index('state_id')
 
 
-    def get_states_by_visit_count(n: int = None) -> pd.DataFrame:
-        return self._visits.groupby(['object' ], sort=False)['batch_idx'].count().reset_index(name='count').sort_values(['count'], ascending=False).head(n).join(self._states, rsuffix='_state').rename(columns={'object_state' : 'state'}).drop(['object'], axis=1)
+    def get_states_by_visit_count(self, n: int = None) -> pd.DataFrame:
+        """Retrieves the top-n states by visit count.
 
-    def get_q_values(state_ids: pd.DataFrame) -> 
+        Args:
+          n: The number of states to return.
 
-    # potentially add sampling? 
-# rest are given states, get t, q, target
+        Returns:
+          The top-n states sorted descending by visit count. The
+          corresponding id and visit count are also included. The
+          columns of the dataframe are state_id, state, and 
+          visit_count.
+
+        """        
+        return self._visits.groupby(['object' ], sort=False)['batch_idx'].count().reset_index(name='visit_count').sort_values(['visit_count'], ascending=False).head(n).set_index('object').join(self._states).rename(columns={'object' : 'state', 'id' : 'state_id'})
+
+    def get_td_errors(state_id: int, sample_count: int) -> pd.DataFrame:
+        """Retrieves a sample of the td_error values for the requested state.
+
+        Args:
+          state_id: The state id for which to retrieve td errors across all actions.
+          sample_count: The max number of td error values to return
+            per action, uniformly spaced amongst matching values indepedent
+            of batch_idx.
+
+        """
+        return 
+
+# rest are given state, get t, q, target
+
+# simple tests. port panel!
+
+
         
                 
 
