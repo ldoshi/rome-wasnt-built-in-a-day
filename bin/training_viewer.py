861abef70a74f050db0b6969485674f4d1f5628b
Lyric Doshi <lyric@mit.edu>
2022-03-24 23:16:52 -0700

    checkpoint.

--- bin/training_viewer.py
+++ bin/training_viewer.py
@@ -12,6 +12,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import numpy as np
+import pandas as pd
 
 from typing import Tuple
 
@@ -23,12 +24,19 @@ def equally_spaced_indices(length, n):
     return np.round(np.linspace(0, length - 1, n)).astype(int)
 
 class TrainingPanel:
-    def __init__(self, states_n: int , state_shape: Tuple[int, int], actions_n: int):
-        self._max_points_per_plot = 50
+    def __init__(self, states_n: int , training_history_database: object_log_readers.TrainingHistoryDatabase):
+        """Initializes the viewer plots
+        
+        Args:
+          states_n: The max number of states for which to display debug information.
+          training_history_database: The accessor to the logs that back the debug 
+            data displayed in the viewer.
+
+        """
+        
         self._states_n = states_n
-        self._state_width = state_width
-        self._state_height = state_height
-        self._actions_n = actions_n
+        self._training_history_database = training_history_database
+        self._max_points_per_plot = 50
         self._plot_width = 4
         self._plot_count = 3
 
@@ -63,19 +71,19 @@ class TrainingPanel:
 
             ax.grid(which="minor")
 
-    def _render_states(self, state_training_history):
-        for i in range(min(len(state_training_history), self._states_n)):
+    def _render_states(self, states: pd.DataFrame):
+        for i in range(min(len(states), self._states_n)):
             ax = self._axs[i, 0]
             ax.cla()
-            ax.matshow(state_training_history[i].state, cmap="binary")
-            ax.set_title("Visits: %d" % state_training_history[i].visit_count)
+            ax.matshow(states.iloc[i].state, cmap="binary")
+            ax.set_title(f"Visits: {states.iloc[i].visit_count}")
 
     def _render_series(self, state_training_history, column_index, method, title):
         for i in range(min(len(state_training_history), self._states_n)):
             ax = self._axs[i, column_index]
             ax.cla()
             history = state_training_history[i]
-            for a in range(self._actions_n):
+            for a in range(self._training_history_database.actions_n):
                 xs, ys = getattr(history, method)(a)
                 if len(xs) > self._max_points_per_plot:
                     subsampled_indices_for_plotting = equally_spaced_indices(
@@ -88,16 +96,22 @@ class TrainingPanel:
             ax.set_title(title)
             ax.legend()
 
-    def update_panel(self, state_training_history):
-        self._render_states(state_training_history)
-        self._render_series(state_training_history, 1, "get_q_values", "Q")
-        self._render_series(
-            state_training_history, 2, "get_q_target_values", "Q Target"
-        )
-        self._render_series(state_training_history, 3, "get_td_errors", "TD Error")
+    def update_panel(self, states: pd.DataFrame) -> None:
+        """Updates the viewer to show debug information for the provided states.
+        
+        states: The states dataframe is expected to have 3 columns:
+          state_id, state, and visit_count.
+
+        """
+
+        self._render_states(states)
+#        self._render_series(state_training_history, 1, "get_q_values", "Q")
+#        self._render_series(
+ #           state_training_history, 2, "get_q_target_values", "Q Target"
+ #       )
+ #       self._render_series(state_training_history, 3, "get_td_errors", "TD Error")
         self._fig.canvas.draw()
         self._fig.canvas.flush_events()
-        plt.pause(0.001)
 
     
 def view_training():
@@ -107,15 +121,12 @@ def view_training():
     training_history_database = object_log_readers.TrainingHistoryDatabase(hparams.object_logging_dir)
 
     states_n = 10
-    panel = TrainingPanel(
-        states_n=states_n,
-        state_shape=training_history_database.state_shape,
-        actions_n=training_history_database.actions_n
-    )
+    panel = TrainingPanel(        states_n=states_n,        training_history_database=training_history_database    )
 
     most_visited_states = training_history_database.get_states_by_visit_count(n=states_n)
     panel.update_panel(most_visited_states)
 
+    plt.pause(1000000)
 
 
 if __name__ == "__main__":
