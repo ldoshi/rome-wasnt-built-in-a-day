85ad6015d8020cb1e1ae8746906b4b53e225ebf0
Lyric Doshi <lyric@mit.edu>
2022-03-26 22:29:58 -0700

    Formate.

--- bin/training_viewer.py
+++ bin/training_viewer.py
@@ -20,20 +20,26 @@ from bridger import builder_trainer
 from bridger.logging import log_entry
 from bridger.logging import object_log_readers
 
+
 def equally_spaced_indices(length, n):
     return np.round(np.linspace(0, length - 1, n)).astype(int)
 
+
 class TrainingPanel:
-    def __init__(self, states_n: int , training_history_database: object_log_readers.TrainingHistoryDatabase):
+    def __init__(
+        self,
+        states_n: int,
+        training_history_database: object_log_readers.TrainingHistoryDatabase,
+    ):
         """Initializes the viewer plots
-        
+
         Args:
           states_n: The max number of states for which to display debug information.
-          training_history_database: The accessor to the logs that back the debug 
+          training_history_database: The accessor to the logs that back the debug
             data displayed in the viewer.
 
         """
-        
+
         self._states_n = states_n
         self._training_history_database = training_history_database
         self._max_points_per_plot = 50
@@ -52,7 +58,6 @@ class TrainingPanel:
             gridspec_kw=gs_args,
             figsize=([2 * width, 2 * self._states_n]),
         )
-
 
         plt.ion()
         plt.show()
@@ -79,7 +84,14 @@ class TrainingPanel:
             ax.matshow(states.iloc[i].state, cmap="binary")
             ax.set_title(f"Visits: {states.iloc[i].visit_count}")
 
-    def _render_series(self, state_ids: pd.Series, get_data_fn: Callable[[int, int], pd.DataFrame], y_column_name:str, plot_column_index: int, plot_title: str):
+    def _render_series(
+        self,
+        state_ids: pd.Series,
+        get_data_fn: Callable[[int, int], pd.DataFrame],
+        y_column_name: str,
+        plot_column_index: int,
+        plot_title: str,
+    ):
         for i in range(min(len(state_ids), self._states_n)):
             ax = self._axs[i, plot_column_index]
             ax.cla()
@@ -87,21 +99,21 @@ class TrainingPanel:
             state_id = state_ids.iloc[i]
             for action in range(self._training_history_database.actions_n):
                 data = get_data_fn(state_id=state_id, action=action)
-                                
+
                 if len(data) > self._max_points_per_plot:
                     subsampled_indices_for_plotting = equally_spaced_indices(
                         len(data), self._max_points_per_plot
                     )
                     data = data.iloc[subsampled_indices_for_plotting]
 
-                ax.plot(data['batch_idx'], data[y_column_name], label=action)
+                ax.plot(data["batch_idx"], data[y_column_name], label=action)
 
             ax.set_title(plot_title)
             ax.legend()
 
     def update_panel(self, states: pd.DataFrame) -> None:
         """Updates the viewer to show debug information for the provided states.
-        
+
         states: The states dataframe is expected to have 3 columns:
           state_id, state, and visit_count.
 
@@ -109,24 +121,48 @@ class TrainingPanel:
 
         self._render_states(states)
 
-        self._render_series(state_ids=states['state_id'], get_data_fn=self._training_history_database.get_q_values, y_column_name='q_value', plot_column_index=1, plot_title= "Q")
-        self._render_series(state_ids=states['state_id'], get_data_fn=self._training_history_database.get_q_target_values, y_column_name='q_target_value', plot_column_index=2, plot_title= "Q Target")
-        self._render_series(state_ids=states['state_id'], get_data_fn=self._training_history_database.get_td_errors, y_column_name='td_error', plot_column_index=3, plot_title= "TD Error")
+        self._render_series(
+            state_ids=states["state_id"],
+            get_data_fn=self._training_history_database.get_q_values,
+            y_column_name="q_value",
+            plot_column_index=1,
+            plot_title="Q",
+        )
+        self._render_series(
+            state_ids=states["state_id"],
+            get_data_fn=self._training_history_database.get_q_target_values,
+            y_column_name="q_target_value",
+            plot_column_index=2,
+            plot_title="Q Target",
+        )
+        self._render_series(
+            state_ids=states["state_id"],
+            get_data_fn=self._training_history_database.get_td_errors,
+            y_column_name="td_error",
+            plot_column_index=3,
+            plot_title="TD Error",
+        )
 
         self._fig.canvas.draw()
         self._fig.canvas.flush_events()
 
-    
+
 def view_training():
     parser = builder_trainer.get_hyperparam_parser()
     hparams = parser.parse_args()
 
-    training_history_database = object_log_readers.TrainingHistoryDatabase(hparams.object_logging_dir)
+    training_history_database = object_log_readers.TrainingHistoryDatabase(
+        hparams.object_logging_dir
+    )
 
     states_n = 10
-    panel = TrainingPanel(        states_n=states_n,        training_history_database=training_history_database    )
+    panel = TrainingPanel(
+        states_n=states_n, training_history_database=training_history_database
+    )
 
-    most_visited_states = training_history_database.get_states_by_visit_count(n=states_n)
+    most_visited_states = training_history_database.get_states_by_visit_count(
+        n=states_n
+    )
     panel.update_panel(most_visited_states)
 
     plt.pause(1000000)
