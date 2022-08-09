b4371c7b1c97a02669a3984796d9905cb6f1fb30
Lyric Doshi <lyric@mit.edu>
2022-08-08 23:41:13 -0700

    Finish Unit Test.

--- bridger/debug/action_inversion_checker_test.py
+++ bridger/debug/action_inversion_checker_test.py
@@ -25,95 +25,100 @@ def _constant_estimators(nA: int) -> List:
     
 class ActionInversionCheckerTest(unittest.TestCase):
 
-    # @parameterized.expand(
-    #     [
-    #         ("No Early Stopping", []),
-    #         (
-    #             "Early Stopping",
-    #             [
-    #                 EarlyStopping(
-    #                     monitor="val_reward",
-    #                     patience=1,
-    #                     mode="max",
-    #                     strict=True,
-    #                     check_on_train_epoch_end=False,
-    #                 )
-    #             ],
-    #         ),
-    #     ]
-    # )
-
+    def setUp(self):
+        self._env = builder_trainer.make_env(
+            name=_ENV_NAME, width=6, force_standard_config=True
+        )
+        self._constant_estimators = _constant_estimators(self._env.nA)
+        self._policies = [policies.GreedyPolicy(constant_estimator) for constant_estimator in self._constant_estimators]
+    
     def test_check_unknown_state(self):
         """Verifies a state that should not be visited does not report inversion."""
-        env = builder_trainer.make_env(
-            name=_ENV_NAME, width=6, force_standard_config=True
-        )
         actions = [[0, 1], [4, 3]]
-        checker = action_inversion_checker.ActionInversionChecker(env=env,actions=actions)
+        checker = action_inversion_checker.ActionInversionChecker(env=self._env,actions=actions)
 
-        env.reset()
-        env.step(0)
-        state, _, _, _ = env.step(0)
+        self._env.reset()
+        self._env.step(0)
+        state, _, _, _ = self._env.step(0)
 
-        constant_estimators = _constant_estimators(env.nA)
-        for constant_estimator in constant_estimators:
-            self.assertEqual(checker.check(states=[state],policy=policies.GreedyPolicy(constant_estimator)), [])
-        
+        for policy in self._policies:
+            self.assertEqual(checker.check(states=[state],policy=policy), [])
+
     def test_check_converged_and_preferred(self):
         """Verifies management of converged and preferred states."""
-        env = builder_trainer.make_env(
-            name=_ENV_NAME, width=6, force_standard_config=True
-        )
         actions = [[0, 1], [4, 3]]
-        checker = action_inversion_checker.ActionInversionChecker(env=env,actions=actions)
+        checker = action_inversion_checker.ActionInversionChecker(env=self._env,actions=actions)
 
-        state = env.reset()
+        state = self._env.reset()
 
-        constant_estimators = _constant_estimators(env.nA)
         states = [state, state]
         
         # Attempt non-preferred action in non-converged state.       
-        self.assertEqual(checker.check(states=states, policy=policies.GreedyPolicy(constant_estimators[1])), [])
-        self.assertEqual(checker.check(states=states, policy=policies.GreedyPolicy(constant_estimators[1])), [])
+        self.assertEqual(checker.check(states=states, policy=self._policies[1]), [])
+        self.assertEqual(checker.check(states=states, policy=self._policies[1]), [])
 
         # Converge the state.
-        self.assertEqual(checker.check(states=states, policy=policies.GreedyPolicy(constant_estimators[0])), [])
+        self.assertEqual(checker.check(states=states, policy=self._policies[0]), [])
 
         # Attempt a non-preferred action in a converged
         # state. Duplicating the state verifies that each state is
         # checked and reported on separately.
         expected_report = action_inversion_checker.ActionInversionReport(state=state, preferred_actions={0,4}, policy_action=1)
-        self.assertEqual(checker.check(states=states, policy=policies.GreedyPolicy(constant_estimators[1])), [expected_report, expected_report])
+        self.assertEqual(checker.check(states=states, policy=self._policies[1]), [expected_report, expected_report])
 
-        # Attempt a preferred action in a converged state.
-        self.assertEqual(checker.check(states=state, policy=policies.GreedyPolicy(constant_estimators[4])), [])
+        # Attempt varying preferred actions in a converged state.
+        self.assertEqual(checker.check(states=states, policy=self._policies[0]), [])
+        self.assertEqual(checker.check(states=states, policy=self._policies[4]), [])
 
-#        then go piece by piece in check for if conditoins.
-#        THEN, one test to ensure actions is parsed correctly into checker for fancier actions re: preferred actions. that can be independent of how check managers eg converged state 
-
-    def atest_check(self):
-        """Verifies that check manages preferred actions and converged states correctly."""
-        env = builder_trainer.make_env(
-            name=_ENV_NAME, width=6, force_standard_config=True
-        )
-        actions = [[0, 1], [4, 3]]
-        checker = action_inversion_checker.ActionInversionChecker(env=env,actions=actions)
-
-        # Check s
         
-        env.reset()
-        env.step(0)
-        state, _, _, _ = env.step(0)
+    @parameterized.expand(
+        [
+            ("Single column", [[0, 0, 0], [4, 4, 4]]   ),
+            ("Single column raised", [[0, 0, 0, 0], [4, 4, 4, 4]]   ),
+            ("Single column more raised", [[0, 0, 0, 0, 0], [4, 4, 4, 4, 4]]   ),
+        ]
+    )
+    def test_check_actions_parsing(self, name: str, actions: List[List[int]]):
+        """Verifies that different action inputs are parsed differently and correctly."""
+        checker = action_inversion_checker.ActionInversionChecker(env=self._env,actions=actions)
 
-        def _constant_estimator(state) -> torch.Tensor:
-            """Returns a policy that always adds a brick to the left side."""
-            return torch.tensor([1, 0, 0, 0])
+        state = self._env.reset()
+        states = [state]
+        expected_reports = []
         
-        self.assertEqual(checker.check(states=[state],policy=policies.GreedyPolicy(_constant_estimator)), [])
-        
-        
-        
+        for i in range(len(actions[0])):
+            # No error for the newest state. The older states still have errors.
+            self.assertEqual(checker.check(states=states, policy=self._policies[1]), expected_reports)
+            # Converge the state.
+            self.assertEqual(checker.check(states=states, policy=self._policies[0]), [])
+            # Move is no longer ok for any state in states.
+            expected_reports.append(action_inversion_checker.ActionInversionReport(state=state, preferred_actions={0,4}, policy_action=1))
+            self.assertEqual(checker.check(states=states, policy=self._policies[1]), expected_reports)
+            state, _, _, _ = self._env.step(0)
+            states.append(state)
 
+        # Verify that after the last block in the 0 column, the next
+        # block placement does not result in a change in the checker.
+        self.assertEqual(checker.check(states=states, policy=self._policies[1]), expected_reports)
+        self.assertEqual(checker.check(states=states, policy=self._policies[0]), [])
+        self.assertEqual(checker.check(states=states, policy=self._policies[1]), expected_reports)
+
+    def test_check_preferred_actions_ignore_bad_actions(self):
+        """Verifies preferred actions do not include actions that don't change the env."""
+        actions = [[0, 0], [3, 3]]
+        checker = action_inversion_checker.ActionInversionChecker(env=self._env,actions=actions)
+
+        state = self._env.reset()
+        self.assertEqual(checker.check(states=[state], policy=self._policies[0]), [])
+        self.assertEqual(checker.check(states=[state], policy=self._policies[1]), [            action_inversion_checker.ActionInversionReport(state=state, preferred_actions={0}, policy_action=1)])
+
+    def test_check_illegal_actions(self):
+        actions = [[0, 1]]
+        self.assertRaisesRegex(ValueError, "Actions must contain exactly 2",action_inversion_checker.ActionInversionChecker, env=self._env,actions=actions)
+
+        actions = [[0, 1], [4, 3], [4, 3]]
+        self.assertRaisesRegex(ValueError, "Actions must contain exactly 2",action_inversion_checker.ActionInversionChecker, env=self._env,actions=actions)   
+                
 
 if __name__ == "__main__":
     unittest.main()
