f5cb7a7cf13d1ecb2de8b17eecc116914930b096
ldoshi <lyric@mit.edu>
2022-09-26 00:03:11 -0700

    Smoke tests training batch (#147)
    Refactor the training batch analyzer:
    
    * Update documentation style
    * Separate main call that parses args and "library" call that executes the work. Today we don't test the library portion of the tool in detail (fine), but we could break it out in the future with a unit test file if useful.
    * Shorten flag names
    
    Add first set of smoke tests for the tools. These tests don't test the tool functionality in detail. They ensure the tool runs according to the expected usage and should break if some fundamental formats or assumptions are violated in the future. This gives us light protection against tool rot as the code base and logging evolves over time.

--- tools/training_batch_comparison_tool.py
+++ tools/training_batch_comparison_tool.py
@@ -1,3 +1,12 @@
+"""Checks that two log files of training batch entries share the same values
+   across all attributes.
+
+   Usage:
+   $ python -m tools.training_batch_comparison_tool
+     --expected_log object_logging_dir/training_batch
+     --test_log object_logging_dir_2/training_batch
+"""
+
 import argparse
 from bridger.logging import object_log_readers
 import os
@@ -6,39 +15,31 @@ import torch
 NUM_ERROR_THRESHOLD = 5
 
 
-def main():
-    """Checks that two log files of training batch entries share the same values
+def compare_training_batches(expected_log: str, test_log: str) -> bool:
+    """Compares training batches between two log files.
+
+    Checks that all training batch entries share the same values
     across all attributes.
-    Example to run:
-    $ python -m tools.training_batch_comparison_tool
-      --path_expected_log object_logging_dir/training_batch
-      --path_test_log object_logging_dir_2/training_batch
+
+    Args:
+      expected_log: The filepath to the first TrainingBatchLogEntry
+        log file.
+      test_log: The filepath to the secon TrainingBatchLogEntry log
+        file.
+
+    Returns:
+      True if all batches matched, false otherwise. When false is
+      returned, the errors will be printed to the console.
+
     """
-
-    parser = argparse.ArgumentParser(
-        description="Compare two object log managers for equality."
-    )
-    parser.add_argument(
-        "--path_expected_log",
-        help="The filepath to the first TrainingBatchLogEntry log file.",
-        required=True,
-    )
-    parser.add_argument(
-        "--path_test_log",
-        help="The filepath to the second TrainingBatchLogEntry log file.",
-        required=True,
-    )
-    args = parser.parse_args()
-
     batch_entry_error_counter = 0
 
     for entry_index, (expected_log_batch_entry, test_log_batch_entry) in enumerate(
         zip(
-            object_log_readers.read_object_log(args.path_expected_log),
-            object_log_readers.read_object_log(args.path_test_log),
+            object_log_readers.read_object_log(expected_log),
+            object_log_readers.read_object_log(test_log),
         )
     ):
-        print(f"Analyzing Entry {entry_index}")
         for field in expected_log_batch_entry.__dataclass_fields__:
             expected_object_log_value = getattr(expected_log_batch_entry, field)
             test_object_log_value = getattr(test_log_batch_entry, field)
@@ -84,7 +85,30 @@ def main():
 
         # Exit early if we hit the maximum number of displayed errors.
         if batch_entry_error_counter >= NUM_ERROR_THRESHOLD:
-            return
+            return False
+
+    return batch_entry_error_counter == 0
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Compare two object log managers for equality."
+    )
+    parser.add_argument(
+        "--expected_log",
+        help="The filepath to the first TrainingBatchLogEntry log file.",
+        required=True,
+    )
+    parser.add_argument(
+        "--test_log",
+        help="The filepath to the second TrainingBatchLogEntry log file.",
+        required=True,
+    )
+    args = parser.parse_args()
+    if compare_training_batches(expected_log=args.expected_log, test_log=args.test_log):
+        print("Success!")
+    else:
+        print("Failed!")
 
 
 if __name__ == "__main__":
