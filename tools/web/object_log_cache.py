963f6d20bf8e84de5ff52c27c52c87eda33003b1
Lyric Doshi <lyric@mit.edu>
2022-10-11 15:04:18 -0700

    Object log cache for efficient access.

---
+++ tools/web/object_log_cache.py
@@ -0,0 +1,116 @@
+"""Cache to load and store objects read in from logs.
+
+The implementation relies on the fact that Flask in development mode
+is single-process. Combined with the GIL, that means we do not expect
+any race conditions to get/load data.
+
+"""
+
+import collections
+import os
+import torch
+
+from typing import Any, Callable, Dict, List
+
+from bridger.logging import log_entry
+from bridger.logging import object_log_readers
+
+STATES_BY_STATE_ID_KEY = "states_by_state_id"
+ACTION_INVERSION_REPORTS_BY_BATCH_ID_KEY = "action_inversion_reports_by_batch_id"
+TRAINING_HISTORY_DATABASE_KEY = "training_history_database_key"
+
+
+def _load_action_inversion_reports_by_batch_id(
+    log_dir: str,
+) -> Dict[int, List[log_entry.ActionInversionReportEntry]]:
+    reports = collections.defaultdict(list)
+
+    log_path = os.path.join(log_dir, log_entry.ACTION_INVERSION_REPORT_ENTRY)
+    for entry in object_log_readers.read_object_log(log_path):
+        reports[entry.batch_idx].append(entry)
+
+    return reports
+
+
+def _load_states_by_state_id(log_dir: str) -> Dict[int, torch.Tensor]:
+    states = {}
+
+    log_path = os.path.join(log_dir, log_entry.STATE_NORMALIZED_LOG_ENTRY)
+    for entry in object_log_readers.read_object_log(log_path):
+        states[entry.id] = entry.object
+
+    return states
+
+
+def _load_training_history_database(
+    log_dir: str,
+) -> object_log_readers.TrainingHistoryDatabase:
+    return object_log_readers.TrainingHistoryDatabase(dirname=log_dir)
+
+
+_LOADERS = {
+    STATES_BY_STATE_ID_KEY: _load_states_by_state_id,
+    ACTION_INVERSION_REPORTS_BY_BATCH_ID_KEY: _load_action_inversion_reports_by_batch_id,
+    TRAINING_HISTORY_DATABASE_KEY: _load_training_history_database,
+}
+
+
+class ObjectLogCache:
+    """Loads and caches logged objects for efficient re-access.
+
+    Attributes:
+      key_hit_counts: Dict of the number of cache hits per key.
+      key_miss_counts: Dict of the number of cache misses per key.
+    """
+
+    def __init__(
+        self, log_dir: str, loaders: Dict[str, Callable[[str], Any]] = _LOADERS
+    ):
+        """Initializes cache.
+
+        Args:
+          log_dir: The directory containing the object log files.
+          loaders: A dict containing a custom load function for each
+            supported key. On cache miss, the load function will be
+            called and its return value will be cached as the value
+            corresponding to the key.
+
+        """
+
+        self._log_dir = log_dir
+        self._cache = {}
+        self._loaders = loaders
+        self.key_hit_counts = collections.defaultdict(int)
+        self.key_miss_counts = collections.defaultdict(int)
+
+    def get(self, key: str) -> Any:
+        """Retrieves the requested data constructed from log entries.
+
+        If the data has not been loaded yet, it will be loaded here
+        first. Each support key type has a custom log entry loader.
+
+        Args:
+          key: The corresponding to the desired log-based data.
+
+        Returns:
+          Logged data loaded into a data structure corresponding to
+          the key. See loader defintions. None if backing files do not
+          exist at the provided location.
+
+        Raises:
+          ValueError on unsupported key.
+
+        """
+        if key not in self._loaders:
+            raise ValueError(f"Unsupported key: {key}")
+
+        if key not in self._cache:
+            self.key_miss_counts[key] += 1
+            try:
+                self._cache[key] = self._loaders[key](self._log_dir)
+            except FileNotFoundError:
+                return None
+        else:
+            self.key_hit_counts[key] += 1
+
+        return self._cache[key]
---
+++ tools/web/object_log_cache_test.py
@@ -0,0 +1,134 @@
+"""Tests for object_log_cache."""
+import unittest
+
+import shutil
+
+from bridger import test_utils
+from bridger.logging import object_logging
+from bridger.logging import object_log_readers
+from bridger.logging import log_entry
+
+from tools.web import object_log_cache
+
+_OBJECT_LOGGING_DIR = "tmp_object_logging_dir"
+
+
+class ObjectLogCacheTest(unittest.TestCase):
+    """Verifies cache loading behavior."""
+
+    def tearDown(self):
+        # TODO: Make a more coherent plan for writing test output to a temp dir
+        #       and retaining it on failure
+        shutil.rmtree("lightning_logs", ignore_errors=True)
+        shutil.rmtree(_OBJECT_LOGGING_DIR, ignore_errors=True)
+
+    def test_get_file_does_not_exist(self):
+        cache = object_log_cache.ObjectLogCache(log_dir=_OBJECT_LOGGING_DIR)
+
+        self.assertIsNone(
+            cache.get(object_log_cache.ACTION_INVERSION_REPORTS_BY_BATCH_ID_KEY)
+        )
+        self.assertEqual(
+            cache.key_miss_counts[
+                object_log_cache.ACTION_INVERSION_REPORTS_BY_BATCH_ID_KEY
+            ],
+            1,
+        )
+        self.assertEqual(
+            cache.key_hit_counts[
+                object_log_cache.ACTION_INVERSION_REPORTS_BY_BATCH_ID_KEY
+            ],
+            0,
+        )
+
+        self.assertIsNone(
+            cache.get(object_log_cache.ACTION_INVERSION_REPORTS_BY_BATCH_ID_KEY)
+        )
+        self.assertEqual(
+            cache.key_miss_counts[
+                object_log_cache.ACTION_INVERSION_REPORTS_BY_BATCH_ID_KEY
+            ],
+            2,
+        )
+        self.assertEqual(
+            cache.key_hit_counts[
+                object_log_cache.ACTION_INVERSION_REPORTS_BY_BATCH_ID_KEY
+            ],
+            0,
+        )
+
+    def test_get_illegal_key(self):
+        cache = object_log_cache.ObjectLogCache(log_dir=_OBJECT_LOGGING_DIR)
+
+        self.assertRaisesRegex(ValueError, "Unsupported", cache.get, key="illegal")
+
+    def test_get_and_load(self):
+        """Verifies cache loads files once."""
+
+        max_steps = 1
+        with object_logging.ObjectLogManager(
+            dirname=_OBJECT_LOGGING_DIR
+        ) as object_log_manager:
+            test_utils.get_trainer(max_steps=max_steps).fit(
+                test_utils.get_model(
+                    object_log_manager=object_log_manager,
+                    debug=True,
+                    env_width=4,
+                    max_episode_length=10,
+                    initial_memories_count=1,
+                )
+            )
+
+        cache = object_log_cache.ObjectLogCache(log_dir=_OBJECT_LOGGING_DIR)
+
+        self.assertIsNotNone(cache.get(object_log_cache.STATES_BY_STATE_ID_KEY))
+        self.assertEqual(
+            cache.key_miss_counts[object_log_cache.STATES_BY_STATE_ID_KEY], 1
+        )
+        self.assertEqual(
+            cache.key_hit_counts[object_log_cache.STATES_BY_STATE_ID_KEY], 0
+        )
+
+        self.assertIsNotNone(cache.get(object_log_cache.STATES_BY_STATE_ID_KEY))
+        self.assertEqual(
+            cache.key_miss_counts[object_log_cache.STATES_BY_STATE_ID_KEY], 1
+        )
+        self.assertEqual(
+            cache.key_hit_counts[object_log_cache.STATES_BY_STATE_ID_KEY], 1
+        )
+
+    def test_loaders_smoke_test(self):
+        """Verifies each loader produces a reasonably shaped result."""
+        max_steps = 4
+        with object_logging.ObjectLogManager(
+            dirname=_OBJECT_LOGGING_DIR
+        ) as object_log_manager:
+            test_utils.get_trainer(max_steps=max_steps).fit(
+                test_utils.get_model(
+                    object_log_manager=object_log_manager,
+                    debug_action_inversion_checker=True,
+                    debug=True,
+                    env_width=4,
+                    max_episode_length=10,
+                    initial_memories_count=1,
+                )
+            )
+
+        cache = object_log_cache.ObjectLogCache(log_dir=_OBJECT_LOGGING_DIR)
+
+        states = cache.get(object_log_cache.STATES_BY_STATE_ID_KEY)
+        self.assertEqual(len(states), 2)
+
+        action_inversion_reports = cache.get(
+            object_log_cache.ACTION_INVERSION_REPORTS_BY_BATCH_ID_KEY
+        )
+        self.assertEqual(len(action_inversion_reports), 1)
+
+        training_history_database = cache.get(
+            object_log_cache.TRAINING_HISTORY_DATABASE_KEY
+        )
+        self.assertEqual(len(training_history_database.get_states_by_visit_count()), 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
