e3d276fdf1759428968839843d0777f3aafe5c5b
Lyric Doshi <lyric@mit.edu>
2022-12-12 21:22:58 -0800

    h

--- bridger/replay_buffer_initializers.py
+++ bridger/replay_buffer_initializers.py
@@ -37,28 +37,45 @@ def _standard_configuration_bridge_states(env: gym.Env) -> Generator[Tuple[Any, 
             env.reset(state)
             next_state, reward, done, _ = env.step(action)
             yield state, action, next_state, reward, done
-                
+
+def _8_bricks(env: gym.Env) -> Generator[Tuple[Any, Any, Any, Any, Any], None, None]:
+    brick_count = 8
+    state_hashes = set()
+    for positions itertools.product(range(env.nA) , repeat=brick_count):
+       state = env.reset()
+
+       for action in positions:
+           then yield each one
     
 STRATEGY_ONLY_RESET_STATE = "only_reset_state"
 STRATEGY_STANDARD_CONFIGURATION_BRIDGE_STATES = "standard_configuration_bridge_states"
+STRATEGY_8_BRICKS = "8_bricks"
         
 _INITIALIZE_REPLAY_BUFFER_BATCH_IDX = -1
                 
 _STRATEGY_MAP = {
     STRATEGY_ONLY_RESET_STATE : _only_reset_state,
     STRATEGY_STANDARD_CONFIGURATION_BRIDGE_STATES : _standard_configuration_bridge_states,
+    STRATEGY_8_BRICKS : _8_bricks
 }
 
     
 
-def initialize_replay_buffer(strategy: str,         env: gym.Env, add_new_experience: Callable[[Any, Any, Any, Any, Any], None], state_visit_logger: Optional[object_logging.OccurrenceLogger] = None):
+def initialize_replay_buffer(strategy: str, replay_buffer_capacity: int,         env: gym.Env, add_new_experience: Callable[[Any, Any, Any, Any, Any], None], state_visit_logger: Optional[object_logging.OccurrenceLogger] = None):
 
     if strategy not in _STRATEGY_MAP:
         raise ValueError(f"Unrecognized replay buffer initialization strategy: {strategy}. Known values are: default, {', '.join(_STRATEGY_MAP.keys())}")
+
+    experience_count = 0
     for (state, action, next_state, reward, done) in _STRATEGY_MAP[strategy](env=env):
         add_new_experience(state, action, next_state, reward, done)
+        experience_count += 1
+        
         if state_visit_logger:
             state_visit_logger.log_occurrence(
                 batch_idx=_INITIALIZE_REPLAY_BUFFER_BATCH_IDX, object=torch.from_numpy(state)
             )
+
+        if experience_count > replay_buffer_capacity:
+            raise ValueError(f"Replay buffer initialized with more experiences than capacity ({replay_buffer_capacity})")
 
