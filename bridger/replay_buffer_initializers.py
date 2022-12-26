4deae527ba2decfc6a8f1d765ec383895c1c1602
ldoshi <lyric@mit.edu>
2022-12-25 19:05:45 -0800

    Seed replay buffer (#166)
    Several strategies to populate the replay buffer before starting training.
    
    This is current intended for debugging. We may use parts of it for intentionally saving memories across trainings in the future.
    
    The following PRs will integrate the strategy into training and improve Sibyl for ease of use.

---
+++ bridger/replay_buffer_initializers.py
@@ -0,0 +1,201 @@
+"""Tools to initialize the replay buffer before beginning training.
+
+When the --initialize_replay_buffer_strategy is set, one common
+use-case is to set --inter-training-steps to 0 and test without
+generating any further experiences.
+
+"""
+
+import gym
+import functools
+import itertools
+import torch
+
+from typing import Any, Callable, Generator, Optional, Tuple
+
+from bridger import builder_trainer, hash_utils
+from bridger.logging import object_logging
+
+
+def _only_reset_state(
+    env: gym.Env,
+) -> Generator[Tuple[Any, Any, Any, Any, Any], None, None]:
+    """Generates every (reset_state, action) pair.
+
+    This is primarily for debugging the full flow from logging to
+    Sibyl.
+
+    Args:
+      env: A gym for executing population strategies. The current
+        implementation assumes that reset() always returns the same
+        starting state.
+
+    Returns:
+      A generator that yields every experience created by following
+      this strategy.
+
+    """
+
+    for action in range(env.nA):
+        state = env.reset()
+        next_state, reward, done, _ = env.step(action)
+        yield state, action, next_state, reward, done
+
+
+def _standard_configuration_bridge_states(
+    env: gym.Env,
+) -> Generator[Tuple[Any, Any, Any, Any, Any], None, None]:
+    """Generates experiences while building the optimal bridge.
+
+    This strategy assumes the current env standard config. Every
+    (state, action) for all action permutations that result in the
+    optimal bridge are enumerated.
+
+    Args:
+      env: A gym for executing population strategies. The current
+        implementation assumes that reset() always returns the same
+        starting state.
+
+    Returns:
+      A generator that yields every experience created by following
+      this strategy.
+
+    """
+
+    build_actions = (
+        builder_trainer.get_action_inversion_checker_actions_standard_configuration(
+            env.nA
+        )
+    )
+    build_actions_substrings = [
+        [build_action[:i] for i in range(len(build_action) + 1)]
+        for build_action in build_actions
+    ]
+
+    build_orderings = list(itertools.product(*build_actions_substrings))
+    # Remove the last member, which represents a completed bridge.
+    build_orderings = build_orderings[:-1]
+
+    for left_actions, right_actions in build_orderings:
+        state = env.reset()
+        for action in left_actions + right_actions:
+            state, _, _, _ = env.step(action)
+
+        for action in range(env.nA):
+            env.reset(state)
+            next_state, reward, done, _ = env.step(action)
+            yield state, action, next_state, reward, done
+
+
+def _n_bricks(
+    brick_count: int, env: gym.Env
+) -> Generator[Tuple[Any, Any, Any, Any, Any], None, None]:
+    """Generates distinct experiences from exhaustively placing n bricks.
+
+    Enumerates every permutation of placing n bricks and removes
+    duplicate (state, action) pairs.
+
+    Args:
+      brick_count: The number of bricks to place.
+      env: A gym for executing population strategies. The current
+        implementation assumes that reset() always returns the same
+        starting state.
+
+    Returns:
+      A generator that yields every experience created by following
+      this strategy.
+    """
+
+    state_action_tuples = set()
+    for episode_actions in itertools.product(range(env.nA), repeat=brick_count):
+        state = env.reset()
+        for action in episode_actions:
+            next_state, reward, done, _ = env.step(action)
+
+            state_action_tuple = (hash_utils.hash_tensor(state), action)
+            if state_action_tuple not in state_action_tuples:
+                state_action_tuples.add(state_action_tuple)
+                yield state, action, next_state, reward, done
+
+            if done:
+                break
+
+            state = next_state
+
+
+STRATEGY_ONLY_RESET_STATE = "only_reset_state"
+STRATEGY_STANDARD_CONFIGURATION_BRIDGE_STATES = "standard_configuration_bridge_states"
+STRATEGY_2_BRICKS = "2_bricks"
+STRATEGY_4_BRICKS = "4_bricks"
+STRATEGY_6_BRICKS = "6_bricks"
+
+_INITIALIZE_REPLAY_BUFFER_BATCH_IDX = -1
+
+_STRATEGY_MAP = {
+    STRATEGY_ONLY_RESET_STATE: _only_reset_state,
+    STRATEGY_STANDARD_CONFIGURATION_BRIDGE_STATES: _standard_configuration_bridge_states,
+    STRATEGY_2_BRICKS: functools.partial(_n_bricks, brick_count=2),
+    STRATEGY_4_BRICKS: functools.partial(_n_bricks, brick_count=4),
+    STRATEGY_6_BRICKS: functools.partial(_n_bricks, brick_count=6),
+}
+
+
+def initialize_replay_buffer(
+    strategy: str,
+    replay_buffer_capacity: int,
+    env: gym.Env,
+    add_new_experience: Callable[[Any, Any, Any, Any, Any, Any], None],
+    state_visit_logger: Optional[object_logging.OccurrenceLogger] = None,
+    state_logger: Optional[object_logging.LoggerAndNormalizer] = None,
+) -> None:
+    """Initializes the replay buffer following the provided strategy.
+
+    Args:
+      strategy: The name of the strategy to follow.
+      replay_buffer_capacity: The capacity of the replay buffer being
+        populated.
+      env: A gym for executing population strategies. The current
+        implementation assumes that reset() always returns the same
+        starting state.
+      add_new_experience: The function to call in order to add an
+        additional experience to the replay buffer.
+      state_visit_logger: If provided, any states traversed by the
+        population policy are logged as visited.
+      state_logger: If provided, provides access to state ids. This
+        must be provided to access state_ids, which in turn must be
+        passed to add_new_experience if the replay buffer is
+        initialzed in debug mode.
+
+    Raises:
+      ValueError: If the strategy generates more experiences than the
+        capacity of the replay buffer or if the strategy does not
+        correspond to a known implementation.
+
+    """
+
+    if strategy not in _STRATEGY_MAP:
+        raise ValueError(
+            f"Unrecognized replay buffer initialization strategy: {strategy}. Known values are: default, {', '.join(_STRATEGY_MAP.keys())}"
+        )
+
+    experience_count = 0
+    for (state, action, next_state, reward, done) in _STRATEGY_MAP[strategy](env=env):
+
+        if state_visit_logger:
+            state_visit_logger.log_occurrence(
+                batch_idx=_INITIALIZE_REPLAY_BUFFER_BATCH_IDX,
+                object=torch.from_numpy(state),
+            )
+
+        state_id = (
+            state_logger.get_logged_object_id(torch.from_numpy(state))
+            if state_logger
+            else None
+        )
+        add_new_experience(state, action, next_state, reward, done, state_id)
+        experience_count += 1
+
+        if experience_count > replay_buffer_capacity:
+            raise ValueError(
+                f"Replay buffer initialized with more experiences than capacity ({replay_buffer_capacity})"
+            )
---
+++ bridger/replay_buffer_initializers_test.py
@@ -0,0 +1,260 @@
+"""Tests replay buffer initializers."""
+import unittest
+
+from typing import Any, NamedTuple
+
+import numpy as np
+import pathlib
+import shutil
+import torch
+
+from bridger import (
+    builder_trainer,
+    hash_utils,
+    replay_buffer,
+    replay_buffer_initializers,
+)
+from bridger.logging import object_logging
+
+
+_ENV_NAME = "gym_bridges.envs:Bridges-v0"
+_ENV_WIDTH = 6
+_REPLAY_BUFFER_CAPACITY = 10000
+
+_TMP_DIR = "tmp/nested_tmp"
+_LOG_FILENAME = "log_filename"
+
+
+class Experience(NamedTuple):
+    start_state: Any
+    action: Any
+    end_state: Any
+    reward: Any
+    success: Any
+    state_id: int
+
+
+def create_temp_dir():
+    path = pathlib.Path(_TMP_DIR)
+    path.mkdir(parents=True, exist_ok=True)
+
+
+def delete_temp_dir():
+    path = pathlib.Path(_TMP_DIR)
+    shutil.rmtree(path.parts[0], ignore_errors=True)
+
+
+class ReplayBufferInitializersTest(unittest.TestCase):
+    def setUp(self):
+        create_temp_dir()
+
+    def tearDown(self):
+        delete_temp_dir()
+
+    def setUp(self):
+        self._replay_buffer = []
+        self._env = builder_trainer.make_env(
+            name=_ENV_NAME, width=_ENV_WIDTH, force_standard_config=True
+        )
+
+    def add_new_experience(
+        self,
+        start_state: Any,
+        action: Any,
+        end_state: Any,
+        reward: Any,
+        success: Any,
+        state_id: int,
+    ):
+        self._replay_buffer.append(
+            Experience(
+                start_state=start_state,
+                action=action,
+                end_state=end_state,
+                reward=reward,
+                success=success,
+                state_id=state_id,
+            )
+        )
+
+    def test_only_reset_state(self):
+        replay_buffer_initializers.initialize_replay_buffer(
+            strategy=replay_buffer_initializers.STRATEGY_ONLY_RESET_STATE,
+            replay_buffer_capacity=_REPLAY_BUFFER_CAPACITY,
+            env=self._env,
+            add_new_experience=self.add_new_experience,
+        )
+
+        self.assertEqual(len(self._replay_buffer), _ENV_WIDTH)
+        reset_state = self._env.reset()
+        for experience, expected_action in zip(self._replay_buffer, range(_ENV_WIDTH)):
+            np.testing.assert_array_equal(experience.start_state, reset_state)
+            self.assertEqual(experience.action, expected_action)
+
+    def test_standard_configuration_bridge_states(self):
+        replay_buffer_initializers.initialize_replay_buffer(
+            strategy=replay_buffer_initializers.STRATEGY_STANDARD_CONFIGURATION_BRIDGE_STATES,
+            replay_buffer_capacity=_REPLAY_BUFFER_CAPACITY,
+            env=self._env,
+            add_new_experience=self.add_new_experience,
+        )
+        self.assertEqual(
+            len(self._replay_buffer),
+            _ENV_WIDTH * ((int((_ENV_WIDTH - 2) / 2) + 1) ** 2 - 1),
+        )
+        count_successes = 0
+        for experience in self._replay_buffer:
+            if experience.success:
+                count_successes += 1
+        self.assertEqual(count_successes, 2)
+
+    def test_2_bricks(self):
+        """Verifies the n-bricks strategy with 2 bricks.
+
+        We chose 2 so we can reason about all the experiences.
+        """
+        replay_buffer_initializers.initialize_replay_buffer(
+            strategy=replay_buffer_initializers.STRATEGY_2_BRICKS,
+            replay_buffer_capacity=_REPLAY_BUFFER_CAPACITY,
+            env=self._env,
+            add_new_experience=self.add_new_experience,
+        )
+        # The first brick produces one of 6 unique experiences:
+        # (initial state, <each action>). We now have 3 distinct
+        # starting points: (a) brick on the left, (b) brick on the
+        # right, and (c) what looks like the initial state. (a) and
+        # (b) will produce an additional 6 unique experiences
+        # each. (c) will not produce any unique experiences.
+        self.assertEqual(len(self._replay_buffer), 18)
+        count_successes = 0
+        for experience in self._replay_buffer:
+            if experience.success:
+                count_successes += 1
+        self.assertEqual(count_successes, 0)
+
+    def test_4_bricks(self):
+        """Verifies the n-bricks strategy with 4 bricks.
+
+        We chose 4 so we can reason about the number of successes.
+        """
+        replay_buffer_initializers.initialize_replay_buffer(
+            strategy=replay_buffer_initializers.STRATEGY_4_BRICKS,
+            replay_buffer_capacity=_REPLAY_BUFFER_CAPACITY,
+            env=self._env,
+            add_new_experience=self.add_new_experience,
+        )
+        # This number is empirically derived.
+        self.assertEqual(len(self._replay_buffer), 132)
+        count_successes = 0
+        for experience in self._replay_buffer:
+            if experience.success:
+                count_successes += 1
+
+        # The 2 success scenarios occur when the last brick is placed
+        # with action 1 after {0, [4,3]} or action 3 after {[0,1],
+        # 4}. The actions in [] must happen in that order relative to
+        # other list members while {} means the order doesn't matter.
+        self.assertEqual(count_successes, 2)
+
+    def test_capacity_too_small(self):
+        self.assertRaisesRegex(
+            ValueError,
+            "initialized with more experiences than capacity",
+            replay_buffer_initializers.initialize_replay_buffer,
+            strategy=replay_buffer_initializers.STRATEGY_2_BRICKS,
+            replay_buffer_capacity=2,
+            env=self._env,
+            add_new_experience=self.add_new_experience,
+        )
+
+    def test_illegal_strategy(self):
+        self.assertRaisesRegex(
+            ValueError,
+            "Unrecognized replay buffer initialization strategy",
+            replay_buffer_initializers.initialize_replay_buffer,
+            strategy="illegal strategy",
+            replay_buffer_capacity=2,
+            env=self._env,
+            add_new_experience=self.add_new_experience,
+        )
+
+    def test_real_replay_buffer(self):
+        buffer = replay_buffer.ReplayBuffer(
+            capacity=_REPLAY_BUFFER_CAPACITY,
+        )
+        replay_buffer_initializers.initialize_replay_buffer(
+            strategy=replay_buffer_initializers.STRATEGY_2_BRICKS,
+            replay_buffer_capacity=_REPLAY_BUFFER_CAPACITY,
+            env=self._env,
+            add_new_experience=buffer.add_new_experience,
+        )
+        self.assertIsNone(buffer.state_histogram)
+
+    def test_real_replay_buffer_extra_logger(self):
+        buffer = replay_buffer.ReplayBuffer(
+            capacity=_REPLAY_BUFFER_CAPACITY,
+        )
+
+        with object_logging.ObjectLogManager(dirname=_TMP_DIR) as logger:
+            state_logger = object_logging.LoggerAndNormalizer(
+                log_filename=_LOG_FILENAME,
+                object_log_manager=logger,
+                log_entry_object_class=torch.Tensor,
+                make_hashable_fn=hash_utils.hash_tensor,
+            )
+
+            self.assertRaisesRegex(
+                AssertionError,
+                "state id should not be passed if the replay buffer",
+                replay_buffer_initializers.initialize_replay_buffer,
+                strategy=replay_buffer_initializers.STRATEGY_2_BRICKS,
+                replay_buffer_capacity=_REPLAY_BUFFER_CAPACITY,
+                env=self._env,
+                add_new_experience=buffer.add_new_experience,
+                state_logger=state_logger,
+            )
+
+    def test_real_replay_buffer_debug(self):
+        buffer = replay_buffer.ReplayBuffer(
+            capacity=_REPLAY_BUFFER_CAPACITY,
+            debug=True,
+        )
+
+        with object_logging.ObjectLogManager(dirname=_TMP_DIR) as logger:
+            state_logger = object_logging.LoggerAndNormalizer(
+                log_filename=_LOG_FILENAME,
+                object_log_manager=logger,
+                log_entry_object_class=torch.Tensor,
+                make_hashable_fn=hash_utils.hash_tensor,
+            )
+
+            replay_buffer_initializers.initialize_replay_buffer(
+                strategy=replay_buffer_initializers.STRATEGY_2_BRICKS,
+                replay_buffer_capacity=_REPLAY_BUFFER_CAPACITY,
+                env=self._env,
+                add_new_experience=buffer.add_new_experience,
+                state_logger=state_logger,
+            )
+
+        # As mentioned in test_2_bricks, there are 3 distinct starting
+        # states.
+        self.assertEqual(len(buffer.state_histogram), 3)
+
+    def test_real_replay_buffer_debug_no_state_id(self):
+        buffer = replay_buffer.ReplayBuffer(
+            capacity=_REPLAY_BUFFER_CAPACITY,
+            debug=True,
+        )
+        self.assertRaisesRegex(
+            AssertionError,
+            "state id must be passed",
+            replay_buffer_initializers.initialize_replay_buffer,
+            strategy=replay_buffer_initializers.STRATEGY_2_BRICKS,
+            replay_buffer_capacity=_REPLAY_BUFFER_CAPACITY,
+            env=self._env,
+            add_new_experience=buffer.add_new_experience,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
