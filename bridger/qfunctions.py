aba8d1a4399b8de494e07ae7f075ff8084aa3834
Lyric Doshi <lyric@mit.edu>
2022-12-30 21:38:37 -0800

    d

--- bridger/qfunctions.py
+++ bridger/qfunctions.py
@@ -11,7 +11,7 @@ import torch.nn.functional as F
 
 from bridger import hash_utils
 
-from typing import Any, Callable, Optional, Sequence, Set
+from typing import Any, Callable, Optional, Set, Tuple
 
 
 def _update_target(tau: float, q: torch.nn.Module, target: torch.nn.Module) -> None:
@@ -206,7 +206,7 @@ def _collect_parameters(
     env: gym.Env,
     hash_fn: Callable[[Any], str],
     remaining_brick_count: int,
-    initial_actions: Sequence[int],
+    initial_actions: Tuple[int],
 ) -> Set[str]:
     state_hashes = set()
     for remaining_actions in itertools.product(
