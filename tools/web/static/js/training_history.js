6012e28da700d4e2cb6280c37ed324a0da6be34d
ldoshi <lyric@mit.edu>
2023-02-13 21:59:54 -0800

    Support refreshable/pasetable URLs for Training History Viewer parameters  (#184)
    Whenever the plots are updated, this PR will store all the parameters (eg start_batch_idx, the state body filter function, etc) into the page URL. Therefore, if the user refreshes the page or copy-pastes the URL to a different tab, the exact same view will be loaded, preserving all the filter parameters. When we have sibyl hosted on a remote server, this means the training history viewer urls can also be shared between people to show the same view.
    
    Main changes:
    * Redefine get_string_or_none and get_int_or_none to return default values.
    * Change the page to accept GET params and set those in the HTML using the template
    * Update the URL whenever new parameters are changed and submitted/applied.
    
    I have not updated the action inversion page at this time. That page is less used and unclear how long it will be useful. I'm opting for a 'update it when it feels necessary' approach.

--- tools/web/static/js/training_history.js
+++ tools/web/static/js/training_history.js
@@ -72,33 +72,20 @@ let _COLORS = [
 
 let _DATA = null;
 
-let _STATE_FILTER = function (state) {
-    return true;
-}
+let _STATE_FILTER = null;
 
 function update_plots() {
-    let experiment_name = $("#experiment-name").val();
-    let start_batch_idx = $("#start-batch-idx").val();
-    let end_batch_idx = $("#end-batch-idx").val();
-    let max_points_per_series = $("#max-points-per-series").val();
-    let number_of_states = $("#number-of-states").val();
+    let plot_params = get_plot_params();
 
     remove_load_error_indicator();
     add_loading_indicator();
 
     $.get(`${_ROOT_URL}training_history_plot_data`,
-	  {
-	      "experiment_name" : experiment_name,
-	      "start_batch_idx" : start_batch_idx,
-	      "end_batch_idx" : end_batch_idx,
-	      "max_points_per_series" : max_points_per_series,
-	      "number_of_states" : number_of_states
-	  },
+	  plot_params,
 	  function(data, response) {
 	      _DATA = data;
 	      render_plots();
-	      $("#current-experiment-name").html(experiment_name);
-
+	      $("#current-experiment-name").html(plot_params['experiment_name']);
     }).fail(function() {
 	add_load_error_indicator();
     }).always(function() {
@@ -111,7 +98,31 @@ function update_state_filter() {
     render_plots();
 }
 
+function get_plot_params() {
+    let experiment_name = $("#experiment-name").val();
+    let start_batch_idx = $("#start-batch-idx").val();
+    let end_batch_idx = $("#end-batch-idx").val();
+    let max_points_per_series = $("#max-points-per-series").val();
+    let number_of_states = $("#number-of-states").val();
+
+    return {
+	"experiment_name" : experiment_name,
+	"start_batch_idx" : start_batch_idx,
+	"end_batch_idx" : end_batch_idx,
+	"max_points_per_series" : max_points_per_series,
+	"number_of_states" : number_of_states,
+    }
+}
+
+function update_url() {
+    let url_params = get_plot_params();
+    url_params["state_filter_function_body"] = $("#state-filter-function-body").val();   
+    window.history.pushState(null, null, _ROOT_URL + "?" + $.param(url_params));
+}
+
 function render_plots() {
+    update_url();
+    
     if (_DATA == null) {
 	return;
     }
