f0165eb578738cbe360bd40d9f38dae22ed4bc9c
Lyric Doshi <lyric@mit.edu>
2023-03-30 22:55:18 -0700

    h

--- tools/web/static/js/replay_buffer_state_counts.js
+++ tools/web/static/js/replay_buffer_state_counts.js
@@ -70,7 +70,14 @@ function update_plots() {
   );
 }
 
+function update_url_params() {
+    let url_param_updates = {'experiment_name' : $("#experiment-name").val() };
+    update_url(url_param_updates);    
+}
+
 function render_plots() {
+    update_url_params();
+    
   // Grab the data from the global _DATA object.
   if (_DATA == null) {
     return;
