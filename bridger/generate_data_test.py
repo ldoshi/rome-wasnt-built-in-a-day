bc7a8bdaf81dbf5c03d278449ae457e44ecfa422
Lyric Doshi <lyric@mit.edu>
2024-01-22 22:20:11 -0800

    Random state generator.

--- bridger/generate_data_test.py
+++ bridger/generate_data_test.py
@@ -3,9 +3,9 @@ import pathlib
 import unittest
 
 from bridger.builder_trainer import make_env
-from bridger.generate_data import DatasetGenerator
+from bridger import generate_data
 
-TMP_LOG_DIR = "tmp_log_dir"
+TMP_LOG_DIR = "tmp_log_dir_10_6_8"
 _ENV_NAME = "gym_bridges.envs:Bridges-v0"
 
 
@@ -24,11 +24,25 @@ class TestGenerateData(unittest.TestCase):
         create_temp_dir()
 
     def test_generate_six_wide(self):
-        dataset = DatasetGenerator(
+        dataset = generate_data.DatasetGenerator(
             log_filename_directory=TMP_LOG_DIR,
-            n_bricks=6,
-            k=4,
-            env=make_env(name=_ENV_NAME, width=6, force_standard_config=True),
+            n_states=None,
+            n_bricks=10,
+            k=6,
+            env=make_env(name=_ENV_NAME, width=8, force_standard_config=True),
+            state_fn=generate_data.n_bricks,
+        )
+        dataset.generate_dataset()
+        dataset.finalize()
+
+    def test_generate_random(self):
+        dataset = generate_data.DatasetGenerator(
+            log_filename_directory=TMP_LOG_DIR,
+            n_bricks=12,
+            n_states=10000,
+            k=6,
+            env=make_env(name=_ENV_NAME, width=8, force_standard_config=True),
+            state_fn=generate_data.n_states,
         )
         dataset.generate_dataset()
         dataset.finalize()
