bd20949f02a517d370cec82b3bfb77293334f39f
lyric <lyric@mit.edu>
2024-08-05 22:58:07 -0700

    One more comment.

--- bridger/builder.py
+++ bridger/builder.py
@@ -164,7 +164,8 @@ class Builder:
           many steps were actually taken. The steps taken may be less
           than episode_length if the construction is successful.
         """
-        state = self._env.reset(initial_state)
+#        state = self._env.reset(initial_state)
+        state = self._env.reset()
         total_reward = 0
         for i in range(episode_length):
             # This lint error seems to be a torch+pylint issue in general.
