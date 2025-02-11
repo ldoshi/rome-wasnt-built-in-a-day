a1bba20e42d2e948f9d0eb299b2993a8fe3090f7
Lint Action <lint-action@samuelmeuli.com>
2025-02-11 05:38:59 +0000

    Fix code style issues with Black

--- bridger/go_explore_phase_1.py
+++ bridger/go_explore_phase_1.py
@@ -327,7 +327,7 @@ def generate_success_entry(
         processes=processes,
     )
 
-    with open('/tmp/state_cache.pkl') as f:
+    with open("/tmp/state_cache.pkl") as f:
         pickle.dump(cache, f)
 
     return success_entries
